71790507df2955c048b290f1c27d8a15f9d233ab
Praveen <praveenkothapally18@gmail.com>
2024-09-13 01:31:27 +0530

    terraform-aws-eks

--- 01-vpc/provider.tf
+++ 01-vpc/provider.tf
@@ -1,16 +1,16 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.31.0" # AWS provider version, not terraform version
     }
   }
 
   backend "s3" {
-    bucket = "daws76s-state-dev"
-    key    = "vpc"
-    region = "us-east-1"
-    dynamodb_table = "daws76s-locking-dev"
+    bucket         = "daws74s-state-dev"
+    key            = "sg"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-locks"
   }
 }
 
